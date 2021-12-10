b60428ab2724f90c3595ee55b3f301b78eaa889e
Rachel Clifton <rachel@Rachels-MacBook-Air.local>
2021-12-10 18:18:19 -0500

    Day 4 part 1

--- advent-of-code-java-starter-master/src/main/java/aoc/App.java
+++ advent-of-code-java-starter-master/src/main/java/aoc/App.java
@@ -6,6 +6,7 @@ package aoc;
 import aoc.day01.Day01;
 import aoc.day02.Day02;
 import aoc.day03.Day03;
+import aoc.day04.Day04;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,6 +27,7 @@ public class App {
         DAYS.put(1, new Day01());
         DAYS.put(2, new Day02());
         DAYS.put(3, new Day03());
+        DAYS.put(4, new Day04());
     }
 
     private static List<String> loadInput(int day){
@@ -43,12 +45,12 @@ public class App {
     }
 
     public static void main(String[] args) {
-        int day = 3;
+        int day = 4;
         if(args.length != 0){
             day = Integer.parseInt(args[0]);
         }
 
-        int part = 2;
+        int part = 1;
         if(args.length > 1){
             part = Integer.parseInt(args[1]);
         }
---
+++ advent-of-code-java-starter-master/src/main/java/aoc/day04/Day04.java
@@ -0,0 +1,116 @@
+package aoc.day04;
+
+import aoc.Day;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day04 implements Day {
+
+    @Override
+    public String part1(List<String> input) {
+        String[] drawnNumbers = input.get(0).split(",");
+        List<Board> boards = new ArrayList<>();
+        for (int i = 2; i < input.size() - 4; i += 6) {
+            boards.add(new Board(input.subList(i, i + 5)));
+        }
+        for (int i = 0; i < 4; i++) {
+            boards = markBoards(drawnNumbers[i], boards);
+        }
+        Board winningBoard = null;
+        int i = 3;
+        while (winningBoard == null) {
+            i++;
+            boards = markBoards(drawnNumbers[i], boards);
+            winningBoard = findWinningBoard(boards);
+        }
+        int sumOfNonMarked = winningBoard.getLines().stream().filter(item -> !item.isDrawn()).map(item -> Integer.parseInt(item.getNumber())).reduce( 0, (subtotal, number) -> subtotal+=number);
+        return String.valueOf(sumOfNonMarked * Integer.parseInt(drawnNumbers[i]));
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return null;
+    }
+
+    private Board findWinningBoard(List<Board> boards) {
+        for(Board board : boards) {
+            List<BoardItem> lines = board.getLines();
+            for(int i = 0; i < lines.size() - 4; i += 5) {
+                if (lines.get(i).isDrawn() && lines.get(i+1).isDrawn() && lines.get(i+2).isDrawn()
+                        &&  lines.get(i+3).isDrawn() &&  lines.get(i+4).isDrawn()) {
+                    return board;
+                }
+            }
+            for(int i = 0; i < 5; i++) {
+                if (lines.get(i).isDrawn() && lines.get(i+5).isDrawn() && lines.get(i+10).isDrawn()
+                        &&  lines.get(i+15).isDrawn() &&  lines.get(i+20).isDrawn()) {
+                    return board;
+                }
+            }
+        }
+        return null;
+    }
+
+    private List<Board> markBoards(String number, List<Board> boards) {
+        for (Board board: boards)
+        {
+            List<BoardItem> newLines = board.getLines().stream().peek(boardItem -> {
+                if(boardItem.getNumber().equals(number)) {
+                    boardItem.setDrawn(true);
+                }
+            }).collect(Collectors.toList());
+            board.setLines(newLines);
+        }
+        return boards;
+    }
+
+    private class Board {
+        private List<BoardItem> lines;
+
+        public Board(List<String> lineList) {
+            lines = createBoardLine(lineList.get(0));
+            lines.addAll(createBoardLine(lineList.get(1)));
+            lines.addAll(createBoardLine(lineList.get(2)));
+            lines.addAll(createBoardLine(lineList.get(3)));
+            lines.addAll(createBoardLine(lineList.get(4)));
+        }
+
+        private List<BoardItem> createBoardLine(String line) {
+            return Arrays.stream(line.split("\\s")).filter(item -> !item.isEmpty()).map(BoardItem::new).collect(Collectors.toList());
+        }
+
+        public List<BoardItem> getLines() {
+            return lines;
+        }
+
+        public void setLines(List<BoardItem> lines) {
+            this.lines = lines;
+        }
+    }
+
+    private class BoardItem {
+        private final String number;
+        private boolean drawn;
+
+        public BoardItem(String number) {
+            this.number = number;
+            drawn = false;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public boolean isDrawn() {
+            return drawn;
+        }
+
+        public void setDrawn(boolean drawn) {
+            this.drawn = drawn;
+        }
+    }
+
+}
