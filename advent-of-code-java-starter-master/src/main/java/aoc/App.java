89da9152f29f6cdbb5b3f638ecf69a82770d6703
Rachel Clifton <rachel@Rachels-Air.lan>
2021-12-12 22:43:32 -0500

    Day 6

--- advent-of-code-java-starter-master/src/main/java/aoc/App.java
+++ advent-of-code-java-starter-master/src/main/java/aoc/App.java
@@ -8,6 +8,7 @@ import aoc.day02.Day02;
 import aoc.day03.Day03;
 import aoc.day04.Day04;
 import aoc.day05.Day05;
+import aoc.day06.Day06;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,6 +31,7 @@ public class App {
         DAYS.put(3, new Day03());
         DAYS.put(4, new Day04());
         DAYS.put(5, new Day05());
+        DAYS.put(6, new Day06());
     }
 
     private static List<String> loadInput(int day){
@@ -47,7 +49,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        int day = 5;
+        int day = 6;
         if(args.length != 0){
             day = Integer.parseInt(args[0]);
         }
---
+++ advent-of-code-java-starter-master/src/main/java/aoc/day06/Day06.java
@@ -0,0 +1,73 @@
+package aoc.day06;
+
+import aoc.Day;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day06 implements Day {
+
+    @Override
+    public String part1(List<String> input) {
+        List<Integer> formattedInput = Arrays.stream(input.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        for (int i = 1; i <= 80; i++) {
+            int numOfNewSpawn = formattedInput.stream().reduce(0, (subtotal, number) -> number == 0 ? subtotal + 1 : subtotal);
+            formattedInput = formattedInput.stream().map(number -> number == 0 ? 6 : number - 1).collect(Collectors.toList());
+            for (int j = 1; j <= numOfNewSpawn; j++) {
+                formattedInput.add(8);
+            }
+        }
+        return String.valueOf(formattedInput.size());
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        List<List<Integer>> totalsAfterHalf = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            totalsAfterHalf.add(fishAfter128(i));
+        }
+
+        List<Integer> formattedInput = Arrays.stream(input.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+        List<Integer> startingTotals = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            int finalI = i;
+            startingTotals.add(formattedInput.stream().reduce(0, (subtotal, number) -> number == finalI ? subtotal + 1 : subtotal));
+        }
+
+        long totalFish = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j ++) {
+                totalFish += (long) startingTotals.get(i) * totalsAfterHalf.get(i).get(j) * totalsAfterHalf.get(j).stream().reduce(0, Integer::sum);
+            }
+        }
+
+        return String.valueOf(totalFish);
+    }
+
+    private List<Integer> fishAfter128(int startingNumber) {
+        List<Integer> day1Spawn = new ArrayList<>();
+        day1Spawn.add(startingNumber);
+        for (int i = 1; i <= 128; i++) {
+            int numOfNewSpawn = day1Spawn.stream().reduce(0, (subtotal, number) -> number == 0 ? subtotal + 1 : subtotal);
+            day1Spawn = day1Spawn.stream().map(number -> number == 0 ? 6 : number - 1).collect(Collectors.toList());
+            for (int j = 1; j <= numOfNewSpawn; j++) {
+                day1Spawn.add(8);
+            }
+        }
+        List<Integer> fishAfter128 = new ArrayList<>();
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 0 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 1 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 2 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 3 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 4 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 5 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 6 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 7 ? subtotal + 1 : subtotal));
+        fishAfter128.add(day1Spawn.stream().reduce(0, (subtotal, number) -> number == 8 ? subtotal + 1 : subtotal));
+        return fishAfter128;
+    }
+
+}
